69ba6deaae04d63494b1b706842600101b85f80c
ZeeshanRamzan06 <zeeshanramzan3f@gmail.com>
2023-04-12 16:30:59 +0500

    RealEstate Code

--- contracts/RealEstate1.sol
+++ contracts/RealEstate1.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 
 pragma solidity >=0.8.2 <0.9.0;
-  // 12,Ali,22,inspector
+  
 contract RealEstate{
 
   /*
@@ -30,6 +30,7 @@ contract RealEstate{
    uint256 CNIC;
    string Email;
    bool isVerified;
+   bool isRegistered;
  }
 /*
     This struct contains the details of Buyer.
@@ -41,6 +42,7 @@ contract RealEstate{
    uint CNIC;
    string Email;
    bool isVerified;
+   bool isRegistered;
  }
  /*
     This struct is contain the details of LandInspector.
@@ -53,16 +55,17 @@ contract RealEstate{
     string designation;
  }
 
-  event Status( string message,address add);
+  event Status( string message,address add);  // 
 //Mappings
   mapping (address => Seller) public sellerMapp; //This mapping is used to store the value of seller.
   mapping (address => Buyer) public buyerMapp; //This mapping is used to store the value of buyer.
   mapping (uint => landDetail) public landMapp; //This mapping is used to store the value of lands.
    
-  
-  
+  /* 
+  this struct type variable store information of landinspector 
+  */
 
-   LandInspectorDetail public landInspecter;  // this struct type variable store information of landinspector
+   LandInspectorDetail public landInspecter;  
    bytes data;  // this state variable for get responce from call function
 
     /*  This modifier is check  LandInspector  allowed  */
@@ -76,6 +79,8 @@ contract RealEstate{
        require (landInspecter.landInspecterAddress != msg.sender ,"landInspector not Allowed");
        _;
    }
+
+  
 //  This contstructor is used for adding the information of landInspecter.
 
    constructor(uint id,string memory name,uint age,string memory _designation){
@@ -107,8 +112,9 @@ contract RealEstate{
      string memory _email
      
   ) public landInspectornotAllowed {
-       require(sellerMapp[msg.sender].isVerified == false, "buyer not Allowed.");
-      sellerMapp[msg.sender] = Seller ( _name,_age,_city,_cnic,_email,false );
+    // This condition check buyer not allowed to register on seller address.
+      require(buyerMapp[msg.sender].isRegistered == false , "Buyyer Not Allowed" );
+      sellerMapp[msg.sender] = Seller ( _name,_age,_city,_cnic,_email,false,true );
      emit Status( "Seller Register Successfuly",msg.sender);
   }
 /**
@@ -129,8 +135,9 @@ contract RealEstate{
       string memory _email
      
       ) public landInspectornotAllowed  {
-       require(buyerMapp[msg.sender].isVerified == false, "Seller not Allowed.");
-       buyerMapp[msg.sender] = Buyer ( _name,_age,_city,_cnic,_email,false );
+        //This condition check seller not allowed to register on buyer address.
+        require(sellerMapp[msg.sender].isRegistered == false , "Seller Not Allowed" );
+       buyerMapp[msg.sender] = Buyer ( _name,_age,_city,_cnic,_email,false,true );
        emit Status( "Buyer Register Successfuly",msg.sender);
    }
   
@@ -143,6 +150,7 @@ contract RealEstate{
     */
 
        function verifySeller(address sellerAddress) OnlyLandInspector  public {
+         // This contion check seller is verified or not.
         require(sellerMapp[sellerAddress].isVerified == false, "Seller is not verified");
          sellerMapp[sellerAddress].isVerified = true;
         emit Status( "Seller Verify Successfuly",msg.sender);
@@ -156,6 +164,8 @@ contract RealEstate{
      * Emits a {Status} event.
     */
         function verifybuyer(address buyerAddress) OnlyLandInspector  public {
+
+          // This contion check buyer is verified or not
         require(buyerMapp[buyerAddress].isVerified == false, "buyer is not verified");
          buyerMapp[buyerAddress].isVerified = true;
         emit Status( "Bayer Verify Successfuly",msg.sender);
@@ -180,7 +190,10 @@ contract RealEstate{
       uint landPrice,
       uint PropertyPID
       )public {
-      require(sellerMapp[msg.sender].isVerified == false, "Seller not verified.");
+        // This condition buyer not allowed.
+      require(buyerMapp[msg.sender].isRegistered == false , "Buyyer Not Allowed" );
+      // This contion check seller is verified or not.
+      require(sellerMapp[msg.sender].isVerified, "Seller not verified or register.");
        landMapp[_landId] = landDetail(_landId,area,city,state,landPrice,PropertyPID,false,msg.sender);
        emit Status( "Land Register Successfuly",msg.sender);
    }
@@ -213,8 +226,9 @@ contract RealEstate{
         uint _cnic,
         string memory _email
         ) public {
-        require(sellerMapp[msg.sender].isVerified == true, "seller is not register or verified");
-       sellerMapp[msg.sender] =Seller ( _name,_age,_city,_cnic,_email,false);
+          //This contion check seller already register or not.
+        require(sellerMapp[msg.sender].isRegistered == true , "Seller Already Registered" );
+       sellerMapp[msg.sender] =Seller ( _name,_age,_city,_cnic,_email,false,true);
        emit Status( "Seller Updated Successfuly",msg.sender);
      }
 
@@ -235,8 +249,9 @@ contract RealEstate{
        uint _cnic,
        string memory _email 
        ) public {
-         require(buyerMapp[msg.sender].isVerified == true, "buyer is not register or verified");
-       buyerMapp[msg.sender] =Buyer ( _name,_age,_city,_cnic,_email,false);
+        //This contion check buyer already register or not.
+       require(buyerMapp[msg.sender].isRegistered == true , "Buyyer Already Registered" );
+       buyerMapp[msg.sender] =Buyer ( _name,_age,_city,_cnic,_email,false,true);
        emit Status( "Buyer Updated Successfuly",msg.sender);
      }
        /**
@@ -290,9 +305,15 @@ contract RealEstate{
      * @param _landId -  for landId
      * Emits a {Status} event.
     */
-     function buyLand(uint  _landId)public payable {
-     require(buyerMapp[msg.sender].isVerified == false, "buyer is not verified or register");    
-     require (landMapp[_landId].LandPrice < 0,"You have insufficient Balance");
+     function buyLand(uint  _landId)public payable landInspectornotAllowed{
+       //This condition check seller not allowed
+     require (sellerMapp[msg.sender].isRegistered == false ," seller not allowed to buy");
+      // This condition check buyer id verified or not
+     require(buyerMapp[msg.sender].isVerified , "buyer is not verified or register"); 
+     // This condition check land is verified or not.   
+     require(landMapp[_landId].isVerified, "land is not verified or register");  
+     // This condition check land amount is valid or not.
+     require (landMapp[_landId].LandPrice  == msg.value,"Enter Valid Amount");
      (bool success,bytes memory _data)=(landMapp[_landId].owner).call{value:msg.value,gas:2000}("");
      landMapp[_landId].owner = msg.sender;
      data =_data;
